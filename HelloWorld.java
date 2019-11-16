a2b2efa03db4d6db48f6e4f9d78739f5799ebd29
cruise01 <xjhe01@163.com>
2019-11-16 08:01:28 -0800

    Create Practice_1.java

---
+++ HelloWorld.java
@@ -0,0 +1,8 @@
+/**
+ * 任务：写一段hellowrld程序并提交到远程仓。
+ * 验收标准：看远程仓是否有这次提交；是否有合适的commit信息。
+ */
+
+public class Practice_1 {
+
+}
---
+++ Practice_1.java
@@ -0,0 +1,10 @@
+/**
+  *任务：
+  *1.创建dev_1分支，在此分支上Practice_1.java文件中的编写以下程序，完成后提交，commit信息为"Finish Practice_1"
+  * https://www.nowcoder.com/practice/c245af6cfdce49ceb5435f649ee14f89?tpId=90&tqId=30946&tPage=2&rp=2&ru=/ta/2018test&qru=/ta/2018test/question-ranking
+  *2.完成后合入master分支；
+  *3.将master分支推动到远程仓。
+  *验收标准：
+  *1.看远程仓的master分支下有没有对应的commit信息。
+  *
+  */
